1cb2e5a0e7e1962ba58d3be6bbdd9bea945216e4
Petrine <petrines@ntnu.no>
2025-11-20 18:53:13 +0100

    STARTA PÅ 2

--- app/src/app/implementations/screwskinematicssolver.cpp
+++ app/src/app/implementations/screwskinematicssolver.cpp
@@ -29,10 +29,27 @@ uint32_t ScrewsKinematicsSolver::joint_count() const
     return m_screws.size();
 }
 
-//TASK: Implement fk_solve using screws.
+//TASK: FIN Implement fk_solve using screws.
 Eigen::Matrix4d ScrewsKinematicsSolver::fk_solve(const Eigen::VectorXd &joint_positions)
 {
-    return Eigen::Matrix4d::Identity();
+    auto [M, S] = space_chain();
+
+    Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+
+    for (int i = 0; i < S.size(); ++i)
+    {
+        const Eigen::VectorXd screw = S[i];
+        double theta = joint_positions[i];
+
+        Eigen::Matrix4d T_i = utility::matrix_exponential(screw, theta);
+
+        T = T * T_i;
+    }
+
+    T = T * M;
+
+    return T;
+    //return Eigen::Matrix4d::Identity();
 }
 
 Eigen::VectorXd ScrewsKinematicsSolver::ik_solve(const Eigen::Matrix4d &t_sd, const Eigen::VectorXd &j0)
@@ -40,7 +57,7 @@ Eigen::VectorXd ScrewsKinematicsSolver::ik_solve(const Eigen::Matrix4d &t_sd, co
     return ik_solve(t_sd, j0, [&](const std::vector<Eigen::VectorXd> &) { return 0u; });
 }
 
-//TASK: Implement ik_solve using screws.
+//TASK: FIN Implement ik_solve using screws.
 Eigen::VectorXd ScrewsKinematicsSolver::ik_solve(const Eigen::Matrix4d &t_sd, const Eigen::VectorXd &j0, const std::function<uint32_t(const std::vector<Eigen::VectorXd> &)> &solution_selector)
 {
     return j0;
@@ -54,6 +71,7 @@ std::pair<Eigen::Matrix4d, std::vector<Eigen::VectorXd>> ScrewsKinematicsSolver:
 //TASK: Implement body_chain(). You can obtain the variables to transform to body frame from space_chain().
 std::pair<Eigen::Matrix4d, std::vector<Eigen::VectorXd>> ScrewsKinematicsSolver::body_chain()
 {
+
     // auto [m, screws] = space_chain();
     return space_chain();
 }
@@ -61,7 +79,28 @@ std::pair<Eigen::Matrix4d, std::vector<Eigen::VectorXd>> ScrewsKinematicsSolver:
 //TASK: Implement space_jacobian() using space_chain()
 Eigen::MatrixXd ScrewsKinematicsSolver::space_jacobian(const Eigen::VectorXd &current_joint_positions)
 {
-    return Eigen::MatrixXd::Identity(current_joint_positions.size(), current_joint_positions.size());
+    auto [M, S] = space_chain();
+
+    uint32_t n = S.size();
+    Eigen::MatrixXd Js(6, n);
+
+    Eigen::Matrix4d T_i_minus_1 = Eigen::Matrix4d::Identity();
+
+    for (uint32_t i = 0; i < n; ++i)
+    {
+        const Eigen::VectorXd S_i = S[i];
+        double theta_i = current_joint_positions[i];
+
+        Eigen::VectorXd Js_i = utility::adjoint_map(S_i, T_i_minus_1);
+
+        Js.col(i) = Js_i;
+
+        Eigen::Matrix4d T_i = utility::matrix_exponential(S_i, theta_i);
+        T_i_minus_1 = T_i_minus_1 * T_i;
+    }
+
+    return Js;
+    //return Eigen::MatrixXd::Identity(current_joint_positions.size(), current_joint_positions.size());
 }
 
 //TASK: Implement body_jacobian() using body_chain()
